d310d1600e1ce6cbb181c09f15a833afa02b543d
Toby Travis <tobytravis@Tobys-MacBook-Pro-2.local>
2023-08-26 18:20:57 +0100

    Beginning to style up

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,23 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'content-section': {
+					odd: 'var(--content-section-bg-odd)',
+					even: 'var(--content-section-bg-even)',
+				},
+				header: {
+					bg: 'var(--header-bg-color)',
+					text: 'var(--header-text-color)',
+				},
+				headers: 'var(--headers-color)',
+			},
+			fontFamily: {
+				'cormorant': ['Cormorant', 'serif'],
+				'proza-libre': ['Proza Libre', 'sans-serif'],
+			},
+		},
 	},
 	plugins: [],
 }
